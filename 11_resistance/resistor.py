f63b4fcf350b4800796db4409b49cf16755f2116
Roboramv2 <sriramkraja@gmail.com>
2021-06-23 15:24:50 +0530

    huge update

--- 11_resistance/resistor.py
+++ 11_resistance/resistor.py
@@ -1,72 +1,78 @@
-import cv2
-import numpy as np
-from scipy import signal
+from tkinter import *
 
-vkernel = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
-lpf = np.array([[1/9, 1/9, 1/9], [1/9, 1/9, 1/9], [1/9, 1/9, 1/9]])
-hpf = np.array([[-1/9, -1/9, -1/9], [-1/9, 8/9, -1/9], [-1/9, -1/9, -1/9]])
-s = (500, 200)
+root = Tk()
+root.geometry("600x400")
+val1 = StringVar(value = "Black")
+val2 = StringVar(value = "Black")
+val3 = StringVar(value = "Black")
+val4 = StringVar(value = "Black")
+val5 = StringVar(value = "Silver")
+list1 = ["Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Grey", "White"]
+list2 = ["Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Grey", "Nil"]
+list3 = ["Silver", "Gold"]
+d = {"Silver": 10, "Gold" : 5, "Black": 0, "Brown" : 1, "Red" : 2, "Orange" : 3, "Yellow" : 4, "Green" : 5, "Blue" : 6, "Violet" : 7, "Grey" : 8, "White" : 9, "Nil" : 20}
+#################################################################################################
+topframe = Frame(root)
+topframe.pack(side=TOP, pady = (15, 0), padx = (15, 15))
+bottomframe = Frame(root)
+bottomframe.pack(side=BOTTOM, pady = (15, 15), padx = (15, 15))
 
-DEBUG = False
-COLOUR_BOUNDS = [
-                [(0, 0, 0)      , (179, 255, 93)  , "BLACK"  , 0 , (0,0,0)       ],    
-                [(0, 90, 10)    , (15, 250, 100)  , "BROWN"  , 1 , (0,51,102)    ],    
-                [(0, 30, 80)    , (10, 255, 200)  , "RED"    , 2 , (0,0,255)     ],
-                [(10, 70, 70)   , (25, 255, 200)  , "ORANGE" , 3 , (0,128,255)   ], 
-                [(30, 170, 100) , (40, 250, 255)  , "YELLOW" , 4 , (0,255,255)   ],
-                [(35, 20, 110)  , (60, 45, 120)   , "GREEN"  , 5 , (0,255,0)     ],  
-                [(65, 0, 85)    , (115, 30, 147)  , "BLUE"   , 6 , (255,0,0)     ],  
-                [(120, 40, 100) , (140, 250, 220) , "PURPLE" , 7 , (255,0,127)   ], 
-                [(0, 0, 50)     , (179, 50, 80)   , "GRAY"   , 8 , (128,128,128) ],      
-                [(0, 0, 90)     , (179, 15, 250)  , "WHITE"  , 9 , (255,255,255) ],
-                ];
+column1 = Frame(topframe)
+column1.pack(side=LEFT)
+column2 = Frame(topframe)
+column2.pack(side=LEFT)
+column3 = Frame(topframe)
+column3.pack(side=LEFT)
+column4 = Frame(topframe)
+column4.pack(side=LEFT)
+column5 = Frame(topframe)
+column5.pack(side=LEFT)
 
-def findResistors(liveimg):
-    gliveimg = cv2.cvtColor(liveimg, cv2.COLOR_BGR2GRAY)
-    resClose = []
-    rectCascade = cv2.CascadeClassifier("haarcascade_resistors_0.xml")
-    #detect resistors in main frame
-    ressFind = rectCascade.detectMultiScale(gliveimg,1.1,25)
-    for (x,y,w,h) in ressFind: #SWITCH TO H,W FOR <CV3
-        
-        roi_gray = gliveimg[y:y+h, x:x+w]
-        roi_color = liveimg[y:y+h, x:x+w]
+info = Frame(bottomframe)
+info.pack(side = LEFT)
+interface = Frame(bottomframe)
+interface.pack(side = RIGHT)
+########################################################################################################
+def translate(textlist):
+    numlist = list()
+    for i in textlist:
+        numlist.append(d[i])
+    return numlist
 
-        #apply another detection to filter false positives
-        secondPass = rectCascade.detectMultiScale(roi_gray,1.01,5)
+def calc(numlist):
+    out = "The resistance is: "+str(numlist[0])+str(numlist[1])
+    if numlist[3]!=20:
+        out = out+str(numlist[2])+"x10^"+str(numlist[3])+" ohms with a tolerance of +/-"
+    else:
+        out = out+"x10^"+str(numlist[2])+" ohms with a tolerance of +/-"
+    out = out+str(numlist[4])+"%"
+    return out
 
-        if (len(secondPass) != 0):
-            resClose.append((np.copy(roi_color),(x,y,w,h)))
-    return resClose
+def main():
+    colors = [str(val1.get()), str(val2.get()), str(val3.get()), str(val4.get()), str(val5.get())]
+    colorcodes = translate(colors)
+    out = calc(colorcodes)
+    output.config(text = out)
+############################################################################################################
+for i in list1:
+    Radiobutton(column1, text = i, variable = val1, value = i).pack(anchor = W)
+for i in list1:
+    Radiobutton(column2, text = i, variable = val2, value = i).pack(anchor = W)
+for i in list1:
+    Radiobutton(column3, text = i, variable = val3, value = i).pack(anchor = W)
+for i in list2:
+    Radiobutton(column4, text = i, variable = val4, value = i).pack(anchor = W)
+for i in list3:
+    Radiobutton(column5, text = i, variable = val5, value = i).pack(anchor = W)
 
-def process(img):
-    img = signal.convolve2d(img, hpf, boundary='symm', mode='same')
-    # img = signal.convolve2d(img, lpf, boundary='symm', mode='same')
-    # img = signal.convolve2d(img, lpf, boundary='symm', mode='same')
-    # img = signal.convolve2d(img, lpf, boundary='symm', mode='same')
-    # img = signal.convolve2d(img, lpf, boundary='symm', mode='same')
-    # img = signal.convolve2d(img, vkernel, boundary='symm', mode='same')
-    # img = signal.convolve2d(img, hpf, boundary='symm', mode='same')
-    # img = signal.convolve2d(img, hpf, boundary='symm', mode='same')
-    img = np.asarray(img, np.uint8)
-    return img
+button1 = Button(interface, text = "Calculate", command=main)
+button1.pack(side = TOP)
 
-img = cv2.imread("down.jpg")
-resclose = findResistors(img)
-image = resclose[0][0]
-# mask = process(image)
-# print(image.shape)
-# print(mask.shape)
-gimage = cv2.cvtColor(src=image, code=cv2.COLOR_BGR2GRAY)
-gimage = cv2.resize(gimage, s)
-avg = np.mean(gimage, axis = 0)
-avgmask = np.zeros((s[0], len(avg)), dtype = np.uint8)
-for i in range(avgmask.shape[0]):
-    avgmask[i]= avg
-avgmask = cv2.resize(avgmask, s)
-avgmask = cv2. equalizeHist(avgmask)
-cv2.imwrite("avgmask.jpg", avgmask)
-avgmask = process(avgmask)
+output = Label(interface)
+output.pack(side = BOTTOM)
+output.config(text = "Select colors and press button")
+#############################################################################################################
 
-cv2.imwrite("crop.jpg", gimage)
-cv2.imwrite("mask.jpg", avgmask)
+root.title("Resistance")
+root.mainloop()
+
